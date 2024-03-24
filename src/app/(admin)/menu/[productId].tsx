4a2b8b1472d6db4eb68129676e624b646a4298d9
raulathallah <raultaqi@gmail.com>
2024-03-24 12:48:51 +0700

    delete & update product

--- src/app/(admin)/menu/[productId].tsx
+++ src/app/(admin)/menu/[productId].tsx
@@ -1,6 +1,6 @@
 import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import React from "react";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Link, Stack, useLocalSearchParams } from "expo-router";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/components/ProductListItem";
 import { useState } from "react";
@@ -8,6 +8,8 @@ import Button from "@/components/Button";
 import { useCart } from "@/providers/CartProvider";
 import { PizzaSize } from "@/types";
 import { useRouter } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
@@ -32,6 +34,25 @@ const ProductDetailScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Menu",
+          headerRight: () => (
+            <Link href={`/(admin)/menu/create?productId=${productId}`} asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
       <Stack.Screen options={{ title: product.name }} />
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,26 +1,50 @@
-import { View, Text, StyleSheet, TextInput, Image } from "react-native";
+import { View, Text, StyleSheet, TextInput, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import Button from "@/components/Button";
 import { defaultPizzaImage } from "@/components/ProductListItem";
 import Colors from "@/constants/Colors";
 import * as ImagePicker from "expo-image-picker";
-import { Stack } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [error, setError] = useState("");
   const [image, setImage] = useState<string | null>(null);
-
+  const { productId } = useLocalSearchParams();
+  const isUpdating = !!productId;
   const onCreate = () => {
-    <Stack.Screen options={{ title: "Create" }} />;
-
-    console.warn("Creating Product");
+    console.warn("create");
     if (!validateInput()) {
       return;
     }
 
     resetFields();
+  };
+  const onUpdate = () => {
+    console.warn("update");
+
+    if (!validateInput()) {
+      return;
+    }
+
+    resetFields();
+  };
+  const onSubmit = () => {
+    if (isUpdating) {
+      onUpdate();
+    } else {
+      onCreate();
+    }
+  };
+  const onDelete = () => {
+    console.warn("DELETE!!!");
+  };
+  const confirmDelete = () => {
+    Alert.alert("Confirm", "Are you sure want to delete this product", [
+      { text: "Cancel" },
+      { text: "Delete", style: "destructive", onPress: onDelete },
+    ]);
   };
   const resetFields = () => {
     setName("");
@@ -60,6 +84,10 @@ const CreateProductScreen = () => {
   };
   return (
     <View style={styles.container}>
+      <Stack.Screen
+        options={{ title: isUpdating ? "Update Product" : "Create Product" }}
+      />
+
       <Image
         style={styles.image}
         source={{ uri: image || defaultPizzaImage }}
@@ -82,9 +110,13 @@ const CreateProductScreen = () => {
         value={price}
         onChangeText={setPrice}
       />
-
       <Text style={{ color: "red" }}>{error}</Text>
-      <Button text="Create" onPress={onCreate} />
+      <Button text={isUpdating ? "Update" : "Create"} onPress={onSubmit} />
+      {isUpdating && (
+        <Text onPress={confirmDelete} style={styles.textButton}>
+          Delete
+        </Text>
+      )}
     </View>
   );
 };
